9136d948da781e02ff11d7642a3a40892706fe92
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-09-27 23:53:58 +0600

    insert controller add

--- controller/tourController.js
+++ controller/tourController.js
@@ -27,6 +27,29 @@ exports.insertTour = async (req, res, next) => {
     }
 }
 
+// insert tour 
+exports.insertSingleTour = async (req, res, next) => {
+    try {
+        const tour = new Tour(req.body)
+        const result = await tour.save()
+        if (result) {
+            res.status(200).json({
+                message: 'Success',
+                status: true,
+                data: result
+            })
+        }
+    } catch (error) {
+        next(error)
+        res.status(400).json({
+            message: 'Couldnt insert the data',
+            status: false,
+            error: error.message
+
+        })
+    }
+}
+
 // get all tour 
 exports.getAllTour = async (req, res, next) => {
     try {
@@ -55,14 +78,11 @@ exports.getSortTour = async (req, res, next) => {
     try {
 
         let sortStr;
-
         if (req.query.sort) {
             sortStr = req.query.sort.split(',').join(' ')
         }
         const { limit = 4, page = 1 } = req.query
-        console.log(req.query)
         const count = await Tour.countDocuments()
-        console.log(+limit * Number(page))
         const pages = Math.ceil(count / 3)
         const result = await Tour.find()
             .limit(limit)
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -4,6 +4,7 @@ const router=express.Router()
 const tourController=require('../controller/tourController')
 
 router.post('/insert_tour',tourController.insertTour)
+router.post('/insert_single_tour',tourController.insertSingleTour)
 router.get('/tour',tourController.getAllTour)
 router.get('/sort_tour',tourController.getSortTour)
 module.exports=router
