663023b4176e5216205ce20099d8b02ea818e3a8
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-09-27 23:49:43 +0600

    pagination add

--- controller/tourController.js
+++ controller/tourController.js
@@ -30,7 +30,7 @@ exports.insertTour = async (req, res, next) => {
 // get all tour 
 exports.getAllTour = async (req, res, next) => {
     try {
-       
+
         const result = await Tour.find()
         if (result) {
             res.status(200).json({
@@ -53,13 +53,21 @@ exports.getAllTour = async (req, res, next) => {
 // get all tour 
 exports.getSortTour = async (req, res, next) => {
     try {
-       
+
         let sortStr;
-        if(req.query.sort){
-            sortStr=req.query.sort.split(',').join(' ')
+
+        if (req.query.sort) {
+            sortStr = req.query.sort.split(',').join(' ')
         }
-        console.log(sortStr)
-        const result = await Tour.find().sort(sortStr)
+        const { limit = 4, page = 1 } = req.query
+        console.log(req.query)
+        const count = await Tour.countDocuments()
+        console.log(+limit * Number(page))
+        const pages = Math.ceil(count / 3)
+        const result = await Tour.find()
+            .limit(limit)
+            .skip(+limit * Number(page))
+        // .sort(sortStr)
         if (result) {
             res.status(200).json({
                 message: 'Success',
